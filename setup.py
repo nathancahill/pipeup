e0ba6726be0fa06a9fdb1a6c43b1d3f0d6d0776f
Nathan Cahill <nathan@nathancahill.com>
2015-05-15 13:51:19 -0600

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pipeup',
-    version='0.1.1',
+    version='0.1.2',
     packages=['pipeup'],
     install_requires=['click', 'websocket-client'],
     extras_require={
@@ -13,6 +13,7 @@ setup(
     entry_points={
         'console_scripts': ['pipeup=pipeup.cli:main'],
     },
+    download_url='https://github.com/nathancahill/pipeup/tarball/0.1.2',
     author='Nathan Cahill',
     author_email='nathan@nathancahill.com',
     url='http://github.com/nathancahill/pipeup',
